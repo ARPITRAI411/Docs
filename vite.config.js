68ac83eb747481fc50aa9dac042a18c3a72cf101
Arpit Rai <raiarpit411@gmail.com>
2025-09-17 01:12:33 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
+  build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          gsap: ['gsap'],
-          utils: ['./src/utils'], // optional, if you have large utils file
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'react-vendor';
+            if (id.includes('gsap')) return 'gsap-vendor';
+            return 'vendor';
+          }
         },
       },
     },
+    chunkSizeWarningLimit: 2000, // Optional: increase warning limit
   },
-})
+});
