44b77c4d8cb5114d8c501dfa96d45355c51bbae5
Arpit Rai <raiarpit411@gmail.com>
2025-09-17 01:10:01 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          gsap: ['gsap'],
+          utils: ['./src/utils'], // optional, if you have large utils file
+        },
+      },
+    },
+  },
 })
